89d60e342fbe425fe53484f6490d28e3f5bf9648
ionb23 <ion.borziac@gmail.com>
2023-03-04 13:00:37 +0000

    adding placeholder stuff in the components to test functionality of react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+import Header from './components/pages/Header';
+import Home from './components/pages/Home';
+import Project from './components/pages/Project';
+import ProjectGallery from './components/pages/ProjectGallery';
+import Contact from './components/pages/Contact';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <NavTabs />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="project" element={<Project />} />
+          <Route path="projectGallery" element={<ProjectGallery />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
